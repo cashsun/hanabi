8a48171dffd01aa7489b92d8a79f86092f7737b7
Cash Sun <mr.cashsun@gmail.com>
2025-05-12 15:52:47 +0100

    feat: implement multi-agent routing strategy in chat API and refactor POST handler

--- ui/app/api/chat/route.ts
+++ ui/app/api/chat/route.ts
@@ -1,15 +1,61 @@
 import {getConfig, getModel, loadConfigToEnv} from '@/lib/config';
 import {getSystemMessages} from '@/lib/systemPrompts';
 import {getMcpTools} from '@/lib/useMcpTools';
-import {jsonSchema, streamText, tool, ToolChoice} from 'ai';
+import {
+	generateObject,
+	jsonSchema,
+	LanguageModelV1,
+	streamText,
+	tool,
+	ToolChoice,
+	UIMessage,
+} from 'ai';
+import {join} from 'path';
 // Allow streaming responses up to 120 seconds
 export const maxDuration = 120;
+
+async function getMultiAgentsStream(
+	model: LanguageModelV1,
+	config: HanabiConfig,
+	body: {messages: UIMessage[]; withAnswerSchema?: boolean},
+) {
+	const multiAgents = config.multiAgents;
+	switch (multiAgents?.strategy) {
+		case 'routing': {
+			const agents = multiAgents.agents;
+			const agentsByTopic = Object.entries(agents).reduce<
+				Record<string, {apiUrl: string; classification: string; name: string}>
+			>((memo, [name, agent]) => {
+				memo[agent.classification] = {...agent, name};
+				return memo;
+			}, {});
+			const topics = Object.keys(agentsByTopic);
+			const {object: classification} = await generateObject({
+				model,
+				output: 'enum',
+				enum: topics,
+				messages: body.messages,
+			});
+			console.log(`⟡ classification: ${classification}`);
+			const targetAgent = agentsByTopic[classification];
+			console.log(`⟡ worker agent: ${targetAgent?.name}`);
+			return await fetch(join(targetAgent.apiUrl, `/chat`), {
+				method: 'POST',
+				body: JSON.stringify(body),
+			});
+		}
+		default:
+			return new Response(
+				`agentsStrategy type: ${multiAgents?.strategy} not valid.`,
+				{status: 500},
+			);
+	}
+}
 
 export async function POST(req: Request) {
 	loadConfigToEnv();
 	const config = getConfig();
 	const systemMessages = getSystemMessages();
-	console.log('using mcps: >>', config.serve?.mcpKeys);
 	let tools = await getMcpTools(config.serve?.mcpKeys);
 	let toolChoice: ToolChoice<any> = 'auto';
 
@@ -27,10 +73,18 @@ export async function POST(req: Request) {
 	const model = getModel(config.defaultModel);
 	if (!model) {
 		return new Response(`No default model found.`, {
-			status: 400,
+			status: 500,
+		});
+	}
+	// enforce multi-agents mode if found in config
+	if (config.multiAgents) {
+		return await getMultiAgentsStream(model, config, {
+			messages,
+			withAnswerSchema,
 		});
 	}
 
+	// single agent mode
 	const result = streamText({
 		model,
 		tools,
@@ -38,6 +92,5 @@ export async function POST(req: Request) {
 		maxSteps: config.maxSteps ?? 10,
 		messages: [...systemMessages, ...messages],
 	});
-
 	return result.toDataStreamResponse();
 }
